6b9f8ba47658b4ef91cf7b9214cff4b461481c9c
Vanpersie-debug <kayitarevanny@gmail.com>
2026-03-08 19:13:39 +0200

    update

--- routes/log.js
+++ routes/log.js
@@ -2,24 +2,21 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-const verifyToken = require("./middleware/AuthMiddleware");
-// const allowRoles = require("./middleware/roleMiddleware");
+const verifyToken = require("../middleware/AuthMiddlewares");
+const allowRoles = require("../middleware/roleMiddleware"); 
 
 router.get(
-"/logs",
-verifyToken,
-allowRoles("SUPER_ADMIN","ADMIN"),
-async (req,res)=>{
-
-const [logs] = await db.query(
-`SELECT * FROM activity_logs
-ORDER BY created_at DESC
-LIMIT 200`
-);
-
-res.json(logs);
-
-}
+  "/logs",
+  verifyToken,
+  allowRoles("SUPER_ADMIN","ADMIN"),
+  async (req, res) => {
+    const [logs] = await db.query(
+      `SELECT * FROM activity_logs
+       ORDER BY created_at DESC
+       LIMIT 200`
+    );
+    res.json(logs);
+  }
 );
 
 module.exports = router;
